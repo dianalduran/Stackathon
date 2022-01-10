8b955631388396b3f9b55afbdcbad1b978c8b99e
dianalduran <dduran.andrade@gmail.com>
2022-01-10 03:31:43 -0600

    feat: animation added

--- script.js
+++ script.js
@@ -2,19 +2,48 @@ init();
 
 function init() {
   const scene = new THREE.Scene();
+  const gui = new dat.GUI();
 
   const fov = 50;
   const aspect = window.innerWidth / window.innerHeight;
   const near = 0.1;
   const far = 1000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 0, 35);
+
+  const cameraZRotation = new THREE.Group();
+  const cameraZPosition = new THREE.Group();
+  const cameraXRotation = new THREE.Group();
+  const cameraYPosition = new THREE.Group();
+  const cameraYRotation = new THREE.Group();
+
+  cameraZRotation.name = "cameraZRotation";
+  cameraZPosition.name = "cameraZPosition";
+  cameraYRotation.name = "cameraYRotation";
+  cameraYPosition.name = "cameraYPosition";
+  cameraXRotation.name = "cameraXRotation";
+
+  cameraZRotation.add(camera);
+  cameraYPosition.add(cameraZRotation);
+  cameraZPosition.add(cameraYPosition);
+  cameraXRotation.add(cameraZPosition);
+  cameraYRotation.add(cameraXRotation);
+  scene.add(cameraYRotation);
+
+  cameraXRotation.rotation.x = 0;
+  cameraYPosition.position.y = 5;
+  cameraZPosition.position.z = 70;
+
+  gui.add(cameraZPosition.position, "z", -10, 100);
+  gui.add(cameraYRotation.rotation, "y", -Math.PI, Math.PI);
+  gui.add(cameraXRotation.rotation, "x", -Math.PI, Math.PI);
+  gui.add(cameraZRotation.rotation, "z", -Math.PI, Math.PI);
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.getElementById("canvas").appendChild(renderer.domElement);
 
   const controls = new THREE.OrbitControls(camera, renderer.domElement);
+  console.log(controls);
   animate(renderer, scene, camera, controls);
 
   const crown = getCrown();
@@ -30,12 +59,11 @@ function init() {
 
   scene.add(crown);
   scene.add(light);
-  //   renderer.render(scene, camera);
 
   //helpers
   //   const axes = new THREE.AxesHelper();
-  // axes.material.depthTest = true;
-  // axes.renderOrder = 1;
+  //   axes.material.depthTest = true;
+  //   axes.renderOrder = 1;
   //   crown.add(axes);
 
   //   const cameraHelper = new THREE.CameraHelper(camera);
@@ -43,20 +71,26 @@ function init() {
 
   const leftButton = document.getElementById("left");
   const rightButton = document.getElementById("right");
+  const animateButton = document.getElementById("animate");
 
   let direction;
+  let animation = false;
   leftButton.addEventListener("click", function () {
     let clock = new THREE.Clock();
     direction = "left";
-    animate(renderer, scene, camera, controls, clock, direction);
+    animate(renderer, scene, camera, controls, animation, clock, direction);
   });
   rightButton.addEventListener("click", function () {
     let clock = new THREE.Clock();
     direction = "right";
-    animate(renderer, scene, camera, controls, clock, direction);
+    animate(renderer, scene, camera, controls, animation, clock, direction);
+  });
+  animateButton.addEventListener("click", function () {
+    let clock = new THREE.Clock();
+    animation = !animation;
+    animate(renderer, scene, camera, controls, animation, clock);
   });
 
-  // responsive design
   function resize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
@@ -86,24 +120,40 @@ function getCrown() {
 //   return sphere;
 // }
 
-function animate(renderer, scene, camera, controls, clock, direction) {
+function animate(
+  renderer,
+  scene,
+  camera,
+  controls,
+  animation,
+  clock,
+  direction
+) {
+  renderer.render(scene, camera);
+  controls.update();
   if (clock) {
     let timeElapsed = clock.getElapsedTime();
-    const crown = scene.getObjectByName("crown");
-
-    if (timeElapsed < 10 && direction === "left") {
-      crown.rotation.z += 0.1;
+    if (animation) {
+      const cameraZPosition = scene.getObjectByName("cameraZPosition");
+      const cameraXRotation = scene.getObjectByName("cameraXRotation");
+      const cameraYPosition = scene.getObjectByName("cameraYPosition");
+      if (cameraZPosition.position.z > -0) {
+        cameraZPosition.position.z -= 0.15;
+      } else if (cameraZPosition.position.z < -0 && timeElapsed < 7.8) {
+        cameraXRotation.position.x -= 0.01;
+      }
+    } else {
+      const crown = scene.getObjectByName("crown");
+      if (timeElapsed < 10 && direction === "left") {
+        crown.rotation.z += 0.01;
+      }
+      if (timeElapsed < 10 && direction === "right") {
+        crown.rotation.z -= 0.01;
+      }
     }
-
-    if (timeElapsed < 10 && direction === "right") {
-      crown.rotation.z -= 0.1;
-    }
-  } else {
-    renderer.render(scene, camera);
-    controls.update();
   }
 
   requestAnimationFrame(function () {
-    animate(renderer, scene, camera, controls, clock, direction);
+    animate(renderer, scene, camera, controls, animation, clock, direction);
   });
 }
